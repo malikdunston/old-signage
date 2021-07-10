2164182c0b9ee746d6f11f4d58a2848c35bc3ce9
Malik Dunston <hello@malikdunston.com>
2021-07-10 09:34:11 -0400

    map comp from ajs

--- src/App.js
+++ src/App.js
@@ -1,70 +1,27 @@
 import { Component } from "react";
-import "./assets/css/index.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-//app-wide components
-import Navigation from "./components/Navigation.js";
-import Menu from "./components/Menu.js";
-// places components
-import Map from "./components/Map.js";
-// content components
-import Directory from "./components/Directory.js";
-import Home from "./routes/Home";
+	import "./assets/css/index.min.css";
+	import { BrowserRouter as Router, Route } from "react-router-dom";
+
+	import Map from "./components/Map.js";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			url: "http://api/",
-			key: "tech",
-			menu: {
-				places: [["campus", "building", "floor", "room"],
-				["campuses", "buildings", "floors", "rooms"]],
-				content: [["customer", "player", "playlist", "item"],
-				["customers", "players", "playlists", "items"]],
-			},
-			loading: true
 		}
-		this.getData = this.getData.bind(this);
-	}
-	async getData(params = {}) {
-		params["institution"] = this.state.key;
-		const url = new URL(this.state.url);
-		Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-		const response = await fetch(url);
-		const data = await response.json();
-		return data
-	}
-	async componentDidMount() {
-	// don't want to have to call inst every time...
-	// known with url.....
-		const data = await this.getData({ children: 1 });
-		this.setState({ institution: data[0], loading: false });
 	}
 	render() {
-		const frame = (thisRoute) => <div>
-			<Navigation data={this.state["institution"]} />
-			<Route exact path="/" component={Home} />
-			<Menu
-				institution={this.state["institution"]}
-				menu={this.state.menu["places"]}
-				pathname={thisRoute.location.pathname.split("/")[1]}
-				params={thisRoute.match.params} />
-		</div>
-		const app = <div className="App">
-			<Router>
-				<Route path="/" render={(thisRoute) => (frame(thisRoute))} />
-				<Route path="/places/:campus?/:building?/:floor?/:room?"
-					render={(route) => (
-						<div id="places">
-							<Map
-								institution={this.state["institution"]}
-								params={route.match.params}
-								getData={this.getData} />
-						</div>
-					)} />
-			</Router>
-		</div>;
-		return this.state.loading ? <div>loading app</div> : app
+		return <Router>
+			<Route exact
+				path="/"
+				render={()=>{
+					return <div>
+						<Map>
+						</Map>
+					</div>
+				}}
+			/>
+		</Router>
 	}
 }
 
